1749137791088caa435f6710d5f63d34b4a5426f
thecoder-001 <ayush06feb@gmail.com>
2023-06-02 12:53:50 +0530

    Basics
    Co-authored-by: APURV <APURV960@users.noreply.github.com>
    Co-authored-by: Aayush Ranjan <aayushxrj@users.noreply.github.com>

---
+++ main-res/scripts.js
@@ -0,0 +1,27 @@
+// Initialize and add the map
+let map;
+
+async function initMap() {
+  // The location of Vellore
+  const position = { lat: 12.934, lng: 79.146 };
+  // Request needed libraries.
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+  // The map, centered at Uluru
+  map = new Map(document.getElementById("map"), {
+    zoom: 14,
+    center: position,
+    mapId: "DEMO_MAP_ID",
+  });
+
+  // The marker, positioned at Uluru
+  const marker = new AdvancedMarkerElement({
+    map: map,
+    position: position,
+    title: "Uluru",
+  });
+}
+
+initMap();
