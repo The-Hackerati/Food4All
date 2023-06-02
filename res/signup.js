2dc92e19c1a0b6a4fdf2cf9849f86d27ea6ee3e9
aayushxrj <aayush.ranjan0904@gmail.com>
2023-06-02 15:56:05 +0530

    added popup authentiation firebase google

--- res/signup.js
+++ res/signup.js
@@ -1,9 +1,6 @@
-import { GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-auth.js";
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-auth.js";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-auth.js";
 
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyB-vWKWiTqYfYwjIIcd_1W1_BLSuibzTb4",
     authDomain: "food4all-427ea.firebaseapp.com",
@@ -11,17 +8,37 @@ const firebaseConfig = {
     storageBucket: "food4all-427ea.appspot.com",
     messagingSenderId: "842713498087",
     appId: "1:842713498087:web:3e5baf4e9a169d0a23bd42"
-  };
-  
-  // Initialize Firebase
+};
+
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
 const provider = new GoogleAuthProvider();
 
-const registerBtn = document.getElementById('registe-button');
+const registerBtn = document.getElementById('register-button');
 const loginBtn = document.getElementById('login-button');
-const email = document.getElementById('email');
-const name = document.getElementById('name');
-const phoneNo = document.getElementById('number');
 
+registerBtn.addEventListener('click', function(event) {
+//   event.preventDefault();
+  const email = document.getElementById('email').value;
+  const name = document.getElementById('name').value;
+  const phoneNo = document.getElementById('number').value;
 
+  var isVerified = true; // Implement verification logic or replace with appropriate condition
+  if (isVerified) {
+    signInWithPopup(auth, provider)
+    .then((result) => {
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    const user = result.user;
+    window.alert("User registered successfully");
+    // ...
+  }).catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    const email = error.customData.email;
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    window.alert("Error: " + errorMessage);
+    // ...
+  });
+  }
+});
